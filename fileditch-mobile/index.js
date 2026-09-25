e133969d69aacd5849254c2a350e17577a138100
rxb3n <61215571+rxb3n@users.noreply.github.com>
2026-09-25 19:08:16 +0200

    Fix root cause: patch FileUtils.maxFileSize/getUserMaxFileSize to bypass client-side precompression size gate, hook sendMessage instead of uploadFiles

--- fileditch-mobile/index.js
+++ fileditch-mobile/index.js
@@ -1,6 +1,7 @@
 (function () {
     var findByProps = vendetta.metro.findByProps;
     var instead = vendetta.patcher.instead;
+    var before = vendetta.patcher.before;
 
     var DISCORD_SIZE_LIMIT = 20 * 1024 * 1024;
     var R2_SIZE_LIMIT = 5 * 1024 * 1024 * 1024;
@@ -11,11 +12,8 @@
         var url = new URL(UPLOAD_API_URL);
         url.searchParams.set("filename", filename);
         url.searchParams.set("mimeType", mimeType);
-
         return fetch(url.toString()).then(function (res) {
-            if (!res.ok) {
-                throw new Error("Ticket API failed with status " + res.status);
-            }
+            if (!res.ok) throw new Error("Ticket API failed with status " + res.status);
             return res.json();
         });
     }
@@ -32,9 +30,7 @@
                         },
                         body: blob
                     }).then(function (putRes) {
-                        if (!putRes.ok) {
-                            throw new Error("R2 upload failed with status " + putRes.status);
-                        }
+                        if (!putRes.ok) throw new Error("R2 upload failed with status " + putRes.status);
                         return ticket.publicUrl;
                     });
                 });
@@ -47,90 +43,98 @@
     return {
         onLoad: function () {
             console.log("[FileditchMobile] onLoad called");
-            var Messages = findByProps("sendMessage", "uploadFiles");
-            console.log("[FileditchMobile] Messages module found:", !!Messages);
-            if (!Messages) {
-                console.error("[FileditchMobile] Could not locate Messages module");
+
+            var FileUtils = findByProps("maxFileSize", "makeFile");
+            var PremiumUtils = findByProps("getUserMaxFileSize");
+            console.log("[FileditchMobile] FileUtils found:", !!FileUtils, "PremiumUtils found:", !!PremiumUtils);
+
+            if (FileUtils && typeof FileUtils.maxFileSize === "function") {
+                patches.push(
+                    instead("maxFileSize", FileUtils, function () {
+                        console.log("[FileditchMobile] maxFileSize() called, returning R2_SIZE_LIMIT");
+                        return R2_SIZE_LIMIT;
+                    })
+                );
+                console.log("[FileditchMobile] Patched FileUtils.maxFileSize");
+            }
+
+            if (PremiumUtils && typeof PremiumUtils.getUserMaxFileSize === "function") {
+                patches.push(
+                    instead("getUserMaxFileSize", PremiumUtils, function () {
+                        console.log("[FileditchMobile] getUserMaxFileSize() called, returning R2_SIZE_LIMIT");
+                        return R2_SIZE_LIMIT;
+                    })
+                );
+                console.log("[FileditchMobile] Patched PremiumUtils.getUserMaxFileSize");
+            }
+
+            var MessageSender = findByProps("sendMessage", "editMessage");
+            console.log("[FileditchMobile] MessageSender found:", !!MessageSender);
+            if (!MessageSender) {
+                console.error("[FileditchMobile] Could not locate MessageSender module");
                 return;
             }
 
-            console.log("[FileditchMobile] Messages.uploadFiles type:", typeof Messages.uploadFiles);
-
-            if (typeof Messages.uploadFiles === "function") {
-                console.log("[FileditchMobile] Patching uploadFiles...");
-                patches.push(
-                    instead("uploadFiles", Messages, function (args, orig) {
-                        try {
-                            var dbgFiles = Array.isArray(args[2]) ? args[2].map(function (f) {
-                                return { name: f.name || f.filename, size: f.size || f.fileSize };
-                            }) : [];
-                            console.log("[FileditchMobile] uploadFiles intercepted. channelId=" + args[0] + " files=" + JSON.stringify(dbgFiles));
-                        } catch (logErr) {
-                            console.log("[FileditchMobile] uploadFiles intercepted (log failed): " + logErr);
-                        }
-
+            patches.push(
+                before("sendMessage", MessageSender, function (args) {
+                    try {
                         var channelId = args[0];
-                        var parsedMessage = args[1];
-                        var files = args[2];
-                        if (!Array.isArray(files) || files.length === 0) {
-                            console.log("[FileditchMobile] No files array, passing through to orig");
-                            return orig.apply(null, args);
-                        }
+                        var message = args[1];
+                        var files = (message && (message.files || message.attachments)) || [];
+                        console.log("[FileditchMobile] sendMessage before-hook. files=" + files.length);
+                        if (!Array.isArray(files) || files.length === 0) return;
 
                         var largeFiles = files.filter(function (f) {
-                            return (f.size || f.fileSize || 0) > DISCORD_SIZE_LIMIT;
+                            var size = f.size || f.fileSize || f.currentSize || 0;
+                            return size > DISCORD_SIZE_LIMIT;
                         });
                         console.log("[FileditchMobile] largeFiles.length=" + largeFiles.length);
-                        if (largeFiles.length === 0) {
-                            return orig.apply(null, args);
+                        if (largeFiles.length === 0) return;
+
+                        if (message.files) {
+                            message.files = message.files.filter(function (f) {
+                                var size = f.size || f.fileSize || f.currentSize || 0;
+                                return size <= DISCORD_SIZE_LIMIT;
+                            });
+                        }
+                        if (message.attachments) {
+                            message.attachments = message.attachments.filter(function (f) {
+                                var size = f.size || f.fileSize || f.currentSize || 0;
+                                return size <= DISCORD_SIZE_LIMIT;
+                            });
                         }
 
-                        var normalFiles = files.filter(function (f) {
-                            return (f.size || f.fileSize || 0) <= DISCORD_SIZE_LIMIT;
-                        });
-
-                        var chain = Promise.resolve();
                         largeFiles.forEach(function (file) {
-                            chain = chain.then(function () {
-                                var fileSize = file.size || file.fileSize || 0;
-                                if (fileSize > R2_SIZE_LIMIT) {
-                                    console.log("[FileditchMobile] File exceeds R2 limit, skipping: " + fileSize);
-                                    return;
-                                }
+                            var fileSize = file.size || file.fileSize || file.currentSize || 0;
+                            if (fileSize > R2_SIZE_LIMIT) {
+                                console.log("[FileditchMobile] File exceeds R2 limit, skipping: " + fileSize);
+                                return;
+                            }
 
-                                var filename = file.name || file.filename || "upload.mp4";
-                                var mimeType = file.type || file.mimeType || "application/octet-stream";
-                                var uri = file.uri || file.url;
+                            var filename = file.name || file.filename || "upload.mp4";
+                            var mimeType = file.type || file.mimeType || "application/octet-stream";
+                            var uri = file.uri || file.url;
 
-                                console.log("[FileditchMobile] Uploading to R2: " + filename + " (" + fileSize + " bytes)");
+                            console.log("[FileditchMobile] Uploading to R2: " + filename + " (" + fileSize + " bytes)");
 
-                                return uploadToR2(uri, filename, mimeType).then(function (publicUrl) {
-                                    console.log("[FileditchMobile] R2 upload success: " + publicUrl);
-                                    var extParts = filename.split(".");
-                                    var ext = extParts.length > 1 ? extParts.pop().toLowerCase() : "";
-                                    var isVideo = mimeType.indexOf("video/") === 0 || !!VIDEO_EXTENSIONS[ext];
+                            uploadToR2(uri, filename, mimeType).then(function (publicUrl) {
+                                console.log("[FileditchMobile] R2 upload success: " + publicUrl);
+                                var extParts = filename.split(".");
+                                var ext = extParts.length > 1 ? extParts.pop().toLowerCase() : "";
+                                var isVideo = mimeType.indexOf("video/") === 0 || !!VIDEO_EXTENSIONS[ext];
+                                var content = isVideo ? ("[\u2800](" + publicUrl + ")") : publicUrl;
 
-                                    var content = isVideo ? ("[\u2800](" + publicUrl + ")") : publicUrl;
-
-                                    return Messages.sendMessage(channelId, { content: content });
-                                }).catch(function (err) {
-                                    console.error("[FileditchMobile] Failed to upload large file:", err);
-                                });
+                                MessageSender.sendMessage(channelId, { content: content });
+                            }).catch(function (err) {
+                                console.error("[FileditchMobile] Failed to upload large file:", err);
                             });
                         });
-
-                        return chain.then(function () {
-                            if (normalFiles.length > 0) {
-                                console.log("[FileditchMobile] Sending " + normalFiles.length + " normal file(s) via orig");
-                                return orig(channelId, parsedMessage, normalFiles);
-                            }
-                        });
-                    })
-                );
-                console.log("[FileditchMobile] Patch applied successfully");
-            } else {
-                console.log("[FileditchMobile] uploadFiles is not a function, patch NOT applied");
-            }
+                    } catch (err) {
+                        console.error("[FileditchMobile] sendMessage hook error:", err);
+                    }
+                })
+            );
+            console.log("[FileditchMobile] Patched sendMessage");
         },
         onUnload: function () {
             for (var i = 0; i < patches.length; i++) patches[i]();
