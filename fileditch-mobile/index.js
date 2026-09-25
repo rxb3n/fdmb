22e621e7a5f75a2e8577aa92464e7241755d7d2e
rxb3n <61215571+rxb3n@users.noreply.github.com>
2026-09-25 18:44:26 +0200

    Fix index.js: wrap entire file as single IIFE expression (loader does return ${plugin.js})

--- fileditch-mobile/index.js
+++ fileditch-mobile/index.js
@@ -1,59 +1,60 @@
-const { findByProps } = vendetta.metro;
-const { instead } = vendetta.patcher;
+(function () {
+    var findByProps = vendetta.metro.findByProps;
+    var instead = vendetta.patcher.instead;
 
-const DISCORD_SIZE_LIMIT = 20 * 1024 * 1024; // 20 MB Discord default
-const R2_SIZE_LIMIT = 5 * 1024 * 1024 * 1024; // 5 GB R2 limit
-const VIDEO_EXTENSIONS = new Set(["mp4", "webm", "mov", "mkv", "avi", "m4v", "flv", "wmv"]);
-const UPLOAD_API_URL = "https://fileditch.vercel.app/api/upload-ticket";
+    var DISCORD_SIZE_LIMIT = 20 * 1024 * 1024;
+    var R2_SIZE_LIMIT = 5 * 1024 * 1024 * 1024;
+    var VIDEO_EXTENSIONS = { mp4: 1, webm: 1, mov: 1, mkv: 1, avi: 1, m4v: 1, flv: 1, wmv: 1 };
+    var UPLOAD_API_URL = "https://fileditch.vercel.app/api/upload-ticket";
 
-async function fetchUploadTicket(filename, mimeType) {
-    const url = new URL(UPLOAD_API_URL);
-    url.searchParams.set("filename", filename);
-    url.searchParams.set("mimeType", mimeType);
+    function fetchUploadTicket(filename, mimeType) {
+        var url = new URL(UPLOAD_API_URL);
+        url.searchParams.set("filename", filename);
+        url.searchParams.set("mimeType", mimeType);
 
-    const res = await fetch(url.toString());
-    if (!res.ok) {
-        throw new Error("Ticket API failed with status " + res.status);
-    }
-    return await res.json();
-}
-
-async function uploadToR2(uri, filename, mimeType) {
-    const ticket = await fetchUploadTicket(filename, mimeType);
-
-    const fileRes = await fetch(uri);
-    const blob = await fileRes.blob();
-
-    const putRes = await fetch(ticket.uploadUrl, {
-        method: "PUT",
-        headers: {
-            "Content-Type": mimeType || "application/octet-stream",
-            "Content-Disposition": "inline"
-        },
-        body: blob
-    });
-
-    if (!putRes.ok) {
-        throw new Error("R2 upload failed with status " + putRes.status);
+        return fetch(url.toString()).then(function (res) {
+            if (!res.ok) {
+                throw new Error("Ticket API failed with status " + res.status);
+            }
+            return res.json();
+        });
     }
 
-    return ticket.publicUrl;
-}
+    function uploadToR2(uri, filename, mimeType) {
+        return fetchUploadTicket(filename, mimeType).then(function (ticket) {
+            return fetch(uri).then(function (fileRes) {
+                return fileRes.blob().then(function (blob) {
+                    return fetch(ticket.uploadUrl, {
+                        method: "PUT",
+                        headers: {
+                            "Content-Type": mimeType || "application/octet-stream",
+                            "Content-Disposition": "inline"
+                        },
+                        body: blob
+                    }).then(function (putRes) {
+                        if (!putRes.ok) {
+                            throw new Error("R2 upload failed with status " + putRes.status);
+                        }
+                        return ticket.publicUrl;
+                    });
+                });
+            });
+        });
+    }
 
-var patches = [];
+    var patches = [];
 
-module.exports = {
-    onLoad: function () {
-        var Messages = findByProps("sendMessage", "uploadFiles");
-        if (!Messages) {
-            console.error("[FileditchMobile] Could not locate Messages module");
-            return;
-        }
+    return {
+        onLoad: function () {
+            var Messages = findByProps("sendMessage", "uploadFiles");
+            if (!Messages) {
+                console.error("[FileditchMobile] Could not locate Messages module");
+                return;
+            }
 
-        if (typeof Messages.uploadFiles === "function") {
-            patches.push(
-                instead("uploadFiles", Messages, function (args, orig) {
-                    return (async () => {
+            if (typeof Messages.uploadFiles === "function") {
+                patches.push(
+                    instead("uploadFiles", Messages, function (args, orig) {
                         var channelId = args[0];
                         var parsedMessage = args[1];
                         var files = args[2];
@@ -72,43 +73,44 @@ module.exports = {
                             return (f.size || f.fileSize || 0) <= DISCORD_SIZE_LIMIT;
                         });
 
-                        for (var i = 0; i < largeFiles.length; i++) {
-                            var file = largeFiles[i];
-                            var fileSize = file.size || file.fileSize || 0;
-                            if (fileSize > R2_SIZE_LIMIT) {
-                                continue;
-                            }
+                        var chain = Promise.resolve();
+                        largeFiles.forEach(function (file) {
+                            chain = chain.then(function () {
+                                var fileSize = file.size || file.fileSize || 0;
+                                if (fileSize > R2_SIZE_LIMIT) {
+                                    return;
+                                }
 
-                            try {
                                 var filename = file.name || file.filename || "upload.mp4";
                                 var mimeType = file.type || file.mimeType || "application/octet-stream";
                                 var uri = file.uri || file.url;
 
-                                var publicUrl = await uploadToR2(uri, filename, mimeType);
-                                var extParts = filename.split(".");
-                                var ext = extParts.length > 1 ? extParts.pop().toLowerCase() : "";
-                                var isVideo = mimeType.indexOf("video/") === 0 || VIDEO_EXTENSIONS.has(ext);
+                                return uploadToR2(uri, filename, mimeType).then(function (publicUrl) {
+                                    var extParts = filename.split(".");
+                                    var ext = extParts.length > 1 ? extParts.pop().toLowerCase() : "";
+                                    var isVideo = mimeType.indexOf("video/") === 0 || !!VIDEO_EXTENSIONS[ext];
 
-                                var content = isVideo ? ("[\u2800](" + publicUrl + ")") : publicUrl;
+                                    var content = isVideo ? ("[\u2800](" + publicUrl + ")") : publicUrl;
 
-                                await Messages.sendMessage(channelId, {
-                                    content: content
+                                    return Messages.sendMessage(channelId, { content: content });
+                                }).catch(function (err) {
+                                    console.error("[FileditchMobile] Failed to upload large file:", err);
                                 });
-                            } catch (err) {
-                                console.error("[FileditchMobile] Failed to upload large file:", err);
-                            }
-                        }
+                            });
+                        });
 
-                        if (normalFiles.length > 0) {
-                            return orig(channelId, parsedMessage, normalFiles);
-                        }
-                    })();
-                })
-            );
+                        return chain.then(function () {
+                            if (normalFiles.length > 0) {
+                                return orig(channelId, parsedMessage, normalFiles);
+                            }
+                        });
+                    })
+                );
+            }
+        },
+        onUnload: function () {
+            for (var i = 0; i < patches.length; i++) patches[i]();
+            patches = [];
         }
-    },
-    onUnload: function () {
-        for (var i = 0; i < patches.length; i++) patches[i]();
-        patches = [];
-    }
-};
+    };
+})()
