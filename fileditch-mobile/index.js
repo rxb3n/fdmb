d3fd0455091b4ae9170c447988e74dd8108804c7
rxb3n <61215571+rxb3n@users.noreply.github.com>
2026-09-25 19:27:12 +0200

    Fix: patch CloudUpload.prototype.reactNativeCompressAndExtractData directly to intercept before native upload attempt, using confirmed live field names (item.uri, currentSize, filename, channelId)

--- fileditch-mobile/index.js
+++ fileditch-mobile/index.js
@@ -1,7 +1,6 @@
 (function () {
     var findByProps = vendetta.metro.findByProps;
     var instead = vendetta.patcher.instead;
-    var before = vendetta.patcher.before;
 
     var DISCORD_SIZE_LIMIT = 20 * 1024 * 1024;
     var R2_SIZE_LIMIT = 5 * 1024 * 1024 * 1024;
@@ -39,105 +38,77 @@
     }
 
     var patches = [];
+    var MessageSender = null;
 
     return {
         onLoad: function () {
             console.log("[FileditchMobile] onLoad called");
 
+            MessageSender = findByProps("sendMessage", "editMessage");
+            console.log("[FileditchMobile] MessageSender found:", !!MessageSender);
+
             var FileUtils = findByProps("maxFileSize", "makeFile");
             var PremiumUtils = findByProps("getUserMaxFileSize");
-            console.log("[FileditchMobile] FileUtils found:", !!FileUtils, "PremiumUtils found:", !!PremiumUtils);
 
             if (FileUtils && typeof FileUtils.maxFileSize === "function") {
-                patches.push(
-                    instead("maxFileSize", FileUtils, function () {
-                        console.log("[FileditchMobile] maxFileSize() called, returning R2_SIZE_LIMIT");
-                        return R2_SIZE_LIMIT;
-                    })
-                );
-                console.log("[FileditchMobile] Patched FileUtils.maxFileSize");
+                patches.push(instead("maxFileSize", FileUtils, function () { return R2_SIZE_LIMIT; }));
             }
-
             if (PremiumUtils && typeof PremiumUtils.getUserMaxFileSize === "function") {
-                patches.push(
-                    instead("getUserMaxFileSize", PremiumUtils, function () {
-                        console.log("[FileditchMobile] getUserMaxFileSize() called, returning R2_SIZE_LIMIT");
-                        return R2_SIZE_LIMIT;
-                    })
-                );
-                console.log("[FileditchMobile] Patched PremiumUtils.getUserMaxFileSize");
+                patches.push(instead("getUserMaxFileSize", PremiumUtils, function () { return R2_SIZE_LIMIT; }));
             }
 
-            var MessageSender = findByProps("sendMessage", "editMessage");
-            console.log("[FileditchMobile] MessageSender found:", !!MessageSender);
-            if (!MessageSender) {
-                console.error("[FileditchMobile] Could not locate MessageSender module");
-                return;
-            }
+            var CloudUploadModule = findByProps("CloudUpload");
+            var CloudUpload = CloudUploadModule && CloudUploadModule.CloudUpload;
+            console.log("[FileditchMobile] CloudUpload found:", !!CloudUpload);
 
-            patches.push(
-                before("sendMessage", MessageSender, function (args) {
-                    try {
-                        var channelId = args[0];
-                        var message = args[1];
-                        var files = (message && (message.files || message.attachments)) || [];
-                        console.log("[FileditchMobile] sendMessage before-hook. files=" + files.length);
-                        if (!Array.isArray(files) || files.length === 0) return;
+            if (CloudUpload && CloudUpload.prototype && CloudUpload.prototype.reactNativeCompressAndExtractData) {
+                var originalCompress = CloudUpload.prototype.reactNativeCompressAndExtractData;
+                console.log("[FileditchMobile] Patching CloudUpload.prototype.reactNativeCompressAndExtractData");
 
-                        var largeFiles = files.filter(function (f) {
-                            var size = f.size || f.fileSize || f.currentSize || 0;
-                            return size > DISCORD_SIZE_LIMIT;
-                        });
-                        console.log("[FileditchMobile] largeFiles.length=" + largeFiles.length);
-                        if (largeFiles.length === 0) return;
+                CloudUpload.prototype.reactNativeCompressAndExtractData = function () {
+                    var file = this;
+                    var size = file.currentSize || file.preCompressionSize || 0;
+                    console.log("[FileditchMobile] compress called. size=" + size + " filename=" + file.filename);
 
-                        if (message.files) {
-                            message.files = message.files.filter(function (f) {
-                                var size = f.size || f.fileSize || f.currentSize || 0;
-                                return size <= DISCORD_SIZE_LIMIT;
-                            });
-                        }
-                        if (message.attachments) {
-                            message.attachments = message.attachments.filter(function (f) {
-                                var size = f.size || f.fileSize || f.currentSize || 0;
-                                return size <= DISCORD_SIZE_LIMIT;
-                            });
-                        }
-
-                        largeFiles.forEach(function (file) {
-                            var fileSize = file.size || file.fileSize || file.currentSize || 0;
-                            if (fileSize > R2_SIZE_LIMIT) {
-                                console.log("[FileditchMobile] File exceeds R2 limit, skipping: " + fileSize);
-                                return;
-                            }
-
-                            var filename = file.name || file.filename || "upload.mp4";
-                            var mimeType = file.type || file.mimeType || "application/octet-stream";
-                            var uri = file.uri || file.url;
-
-                            console.log("[FileditchMobile] Uploading to R2: " + filename + " (" + fileSize + " bytes)");
-
-                            uploadToR2(uri, filename, mimeType).then(function (publicUrl) {
-                                console.log("[FileditchMobile] R2 upload success: " + publicUrl);
-                                var extParts = filename.split(".");
-                                var ext = extParts.length > 1 ? extParts.pop().toLowerCase() : "";
-                                var isVideo = mimeType.indexOf("video/") === 0 || !!VIDEO_EXTENSIONS[ext];
-                                var content = isVideo ? ("[\u2800](" + publicUrl + ")") : publicUrl;
-
-                                MessageSender.sendMessage(channelId, { content: content });
-                            }).catch(function (err) {
-                                console.error("[FileditchMobile] Failed to upload large file:", err);
-                            });
-                        });
-                    } catch (err) {
-                        console.error("[FileditchMobile] sendMessage hook error:", err);
+                    if (size <= DISCORD_SIZE_LIMIT || size > R2_SIZE_LIMIT) {
+                        return originalCompress.apply(file, arguments);
                     }
-                })
-            );
-            console.log("[FileditchMobile] Patched sendMessage");
+
+                    var filename = file.filename || "upload";
+                    var mimeType = file.mimeType || "application/octet-stream";
+                    var uri = (file.item && file.item.uri) || file.uri;
+                    var channelId = file.channelId;
+
+                    console.log("[FileditchMobile] Redirecting oversized file to R2: " + filename + " (" + size + " bytes), uri=" + uri);
+
+                    uploadToR2(uri, filename, mimeType).then(function (publicUrl) {
+                        console.log("[FileditchMobile] R2 upload success: " + publicUrl);
+                        var extParts = filename.split(".");
+                        var ext = extParts.length > 1 ? extParts.pop().toLowerCase() : "";
+                        var isVideo = mimeType.indexOf("video/") === 0 || !!VIDEO_EXTENSIONS[ext];
+                        var content = isVideo ? ("[\u2800](" + publicUrl + ")") : publicUrl;
+                        if (MessageSender) {
+                            MessageSender.sendMessage(channelId, { content: content });
+                        }
+                    }).catch(function (err) {
+                        console.error("[FileditchMobile] Failed to upload large file:", err);
+                    });
+
+                    try { file.status = "CANCELED"; } catch (e) {}
+                    return Promise.reject(new Error("[FileditchMobile] Redirected to R2, skipping native upload"));
+                };
+
+                patches.push(function () {
+                    CloudUpload.prototype.reactNativeCompressAndExtractData = originalCompress;
+                });
+            } else {
+                console.log("[FileditchMobile] CloudUpload.prototype.reactNativeCompressAndExtractData not found, patch NOT applied");
+            }
         },
         onUnload: function () {
-            for (var i = 0; i < patches.length; i++) patches[i]();
+            for (var i = 0; i < patches.length; i++) {
+                try { patches[i](); } catch (e) {}
+            }
             patches = [];
         }
     };
